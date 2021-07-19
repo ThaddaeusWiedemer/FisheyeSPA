957abcb22804bb8ef5e0551b9492e66f889eb0dd
Thaddäus Wiedemer <thaddaeus.wiedemer@gmail.com>
2021-07-19 22:00:09 +0800

    experiments with GPA

---
+++ results/GPA/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person_adaptive.py
@@ -0,0 +1,302 @@
+model = dict(
+    type='TwoStageDetectorAdaptive',
+    pretrained='open-mmlab://detectron2/resnet50_caffe',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='caffe'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHeadAdaptive',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHeadAdaptive',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
+    train_cfg=dict(
+        loss_weight_da=1.0,
+        loss_weight_da_rpn=1.0,
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100)))
+dataset_type = 'CocoDataset'
+data_root_src = 'data/PIROPO/'
+data_root_tgt = 'data/MW-18Mar/'
+classes = ('person', )
+img_norm_cfg = dict(
+    mean=[103.53, 116.28, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline_src = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Normalize',
+        mean=[103.53, 116.28, 123.675],
+        std=[1.0, 1.0, 1.0],
+        to_rgb=False),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+train_pipeline_tgt = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Albu',
+        transforms=[
+            dict(
+                type='ShiftScaleRotate',
+                shift_limit=0.0,
+                scale_limit=0.0,
+                rotate_limit=180,
+                interpolation=1,
+                p=1.0)
+        ],
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap=dict(img='image', gt_masks='masks', gt_bboxes='bboxes'),
+        update_pad_shape=False,
+        skip_img_without_anno=False),
+    dict(
+        type='Normalize',
+        mean=[103.53, 116.28, 123.675],
+        std=[1.0, 1.0, 1.0],
+        to_rgb=False),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[103.53, 116.28, 123.675],
+                std=[1.0, 1.0, 1.0],
+                to_rgb=False),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,
+    train_src=dict(
+        type='CocoDataset',
+        ann_file='/data/COCO/annotations/person_train2017.json',
+        img_prefix='/data/COCO/train2017',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(
+                type='Normalize',
+                mean=[103.53, 116.28, 123.675],
+                std=[1.0, 1.0, 1.0],
+                to_rgb=False),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+        ],
+        classes=('person', )),
+    train_tgt=dict(
+        type='CocoDataset',
+        ann_file='/data/PIROPO/omni_training_1a.json',
+        img_prefix='None',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(
+                type='Albu',
+                transforms=[
+                    dict(
+                        type='ShiftScaleRotate',
+                        shift_limit=0.0,
+                        scale_limit=0.0,
+                        rotate_limit=180,
+                        interpolation=1,
+                        p=1.0)
+                ],
+                bbox_params=dict(
+                    type='BboxParams',
+                    format='pascal_voc',
+                    label_fields=['gt_labels'],
+                    min_visibility=0.0,
+                    filter_lost_elements=True),
+                keymap=dict(img='image', gt_masks='masks', gt_bboxes='bboxes'),
+                update_pad_shape=False,
+                skip_img_without_anno=False),
+            dict(
+                type='Normalize',
+                mean=[103.53, 116.28, 123.675],
+                std=[1.0, 1.0, 1.0],
+                to_rgb=False),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+        ],
+        classes=('person', )),
+    val=dict(
+        type='CocoDataset',
+        ann_file='/data/PIROPO/omni_test2.json',
+        img_prefix='None',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[103.53, 116.28, 123.675],
+                        std=[1.0, 1.0, 1.0],
+                        to_rgb=False),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ],
+        classes=('person', )),
+    test=dict(
+        type='CocoDataset',
+        ann_file='data/MW-18Mar/test.json',
+        img_prefix='data/MW-18Mar/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[103.53, 116.28, 123.675],
+                        std=[1.0, 1.0, 1.0],
+                        to_rgb=False),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ],
+        classes=('person', )))
+evaluation = dict(interval=5, metric='bbox')
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(policy='step', warmup=None, step=[10000])
+runner = dict(type='EpochBasedRunnerAdaptive', max_epochs=12)
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
+custom_hooks = [dict(type='NumClassCheckHook')]
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = 'mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco-person_20201216_175929-d022e227.pth'
+resume_from = None
+workflow = [('train', 1)]
+work_dir = 'work_dirs/GPA/coco_piropo_1a'
+gpu_ids = range(0, 4)
--- train_gpa_coco_piropo.sh
+++ train_gpa_coco_piropo.sh
@@ -14,8 +14,8 @@ TGT_ROOT=omni_training
 TEST_FILE=/data/PIROPO/omni_test2.json
 
 # GPUs
-N_GPU=8 # if this is changed, the number of iterations also needs to be adapted
-VIS_GPU=0,1,2,3,4,5,6,7
+N_GPU=4 # if this is changed, the number of iterations also needs to be adapted
+VIS_GPU=4,5,6,7
 GPU_PORT=29501
 
 # mkdir $WORK_DIR
@@ -33,13 +33,20 @@ GPU_PORT=29501
 #     --out ${WORK_DIR}/${WORK_ROOT}_0/piropo_test2.pkl 2>&1 | tee ${RES_DIR}/${RES_ROOT}_0.txt
 
 # finetuning on n samples per dataset, with 10 datasets labeled a through j per n
-# for n in {1,2,5,10,20,50,100,200,500,1000}; do
-# for n in {100,200,500,1000}; do
-#     for x in {a..j}; do
-        n=100
-        x=a
+for n in {1,2,5,10,20,50,100,200,500,1000}; do
+    for x in {a..j}; do
         _WORK_DIR=${WORK_DIR}/${WORK_ROOT}_${n}${x}
         mkdir ${_WORK_DIR}
+
+        # with all images, we train 12 epochs:
+        #   ceil(2357/16) = 148 iterations --> 148 * 12 = 1776
+        (( iter_per_epoch=($n+16-1)/16 )) # this is a custom implementation of the ceiling function
+        (( epochs=(1776+$iter_per_epoch-1)/$iter_per_epoch ))
+        # with 100 images, we need smaller lr after 6 epochs:
+        #   ceil(100/16) = 7 --> 7 * 6 = 42 --> lr update after 42 iterations
+        (( lr_update_step=(42+$iter_per_epoch-1)/$iter_per_epoch ))
+        # we want to evaluate ca. every 178 iterations (corresponds to ca. 10 times per model)
+        (( val_interval=(178+$iter_per_epoch-1)/$iter_per_epoch ))
 
         # train
         CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_DIR}/dist_train_adaptive.sh \
@@ -52,6 +59,10 @@ GPU_PORT=29501
             data.train_tgt.ann_file=${TGT_DIR}/${TGT_ROOT}_${n}${x}.json \
             data.train_tgt.img_prefix=None \
             data.val.ann_file=${TEST_FILE} \
+            runner.max_epochs=$epochs \
+            lr_config.step=[$lr_update_step] \
+            evaluation.interval=$val_interval \
+            checkpoint_config.interval=2000 \
             data.val.img_prefix=None 2>&1 | tee ${RES_DIR}/${RES_ROOT}_${n}${x}.log
 
         # test
@@ -66,65 +77,45 @@ GPU_PORT=29501
         --out ${_WORK_DIR}/piropo_test2.pkl 2>&1 | tee ${RES_DIR}/${RES_ROOT}_${n}${x}.txt
 
         # free up space
-        # rm ${_WORK_DIR}/*.log
+        rm ${_WORK_DIR}/*.log
+        rm ${_WORK_DIR}/*.pkl
         rm ${_WORK_DIR}/epoch_[1-9].pth
         rm ${_WORK_DIR}/epoch_10.pth
         rm ${_WORK_DIR}/epoch_11.pth
-#     done
-# done
+    done
+done
 
 # finetune once on whole dataset
-# _WORK_DIR=${WORK_DIR}/${WORK_ROOT}_all
-# mkdir ${_WORK_DIR}
+_WORK_DIR=${WORK_DIR}/${WORK_ROOT}_all
+mkdir ${_WORK_DIR}
 
 # # train
-# CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_DIR}/dist_train_adaptive.sh \
-# ${CONFIG_FILE} \
-# ${N_GPU} \
-# --work-dir ${_WORK_DIR} \
-# --cfg-options data.samples_per_gpu=$((16/$N_GPU)) \
-#     data.train_src.ann_file=${SRC_FILE} \
-#     data.train_src.img_prefix=${SRC_DIR} \
-#     data.train_tgt.ann_file=${TGT_DIR}/${TGT_ROOT}.json \
-#     data.train_tgt.img_prefix=None \
-#     data.val.ann_file=${TEST_FILE} \
-#     data.val.img_prefix=None
+CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_DIR}/dist_train_adaptive.sh \
+${CONFIG_FILE} \
+${N_GPU} \
+--work-dir ${_WORK_DIR} \
+--cfg-options data.samples_per_gpu=$((16/$N_GPU)) \
+    data.train_src.ann_file=${SRC_FILE} \
+    data.train_src.img_prefix=${SRC_DIR} \
+    data.train_tgt.ann_file=${TGT_DIR}/${TGT_ROOT}.json \
+    data.train_tgt.img_prefix=None \
+    data.val.ann_file=${TEST_FILE} \
+    data.val.img_prefix=None
 
 # # test
-# CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_DIR}/dist_test.sh \
-# ${CONFIG_FILE} \
-# ${_WORK_DIR}/latest.pth \
-# ${N_GPU} \
-# --eval bbox \
-# --cfg-options data.samples_per_gpu=$((16/$N_GPU)) \
-#     data.test.ann_file=${TEST_FILE} \
-#     data.test.img_prefix=None \
-# --out ${_WORK_DIR}/piropo_test2.pkl 2>&1 | tee ${RES_DIR}/${RES_ROOT}_all.txt
+CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_DIR}/dist_test.sh \
+${CONFIG_FILE} \
+${_WORK_DIR}/latest.pth \
+${N_GPU} \
+--eval bbox \
+--cfg-options data.samples_per_gpu=$((16/$N_GPU)) \
+    data.test.ann_file=${TEST_FILE} \
+    data.test.img_prefix=None \
+--out ${_WORK_DIR}/piropo_test2.pkl 2>&1 | tee ${RES_DIR}/${RES_ROOT}_all.txt
 
 # # free up space
-# rm ${_WORK_DIR}/*.log
-# rm ${_WORK_DIR}/epoch_[1-9].pth
-# rm ${_WORK_DIR}/epoch_10.pth
-# rm ${_WORK_DIR}/epoch_11.pth
-
-
-
-# mkdir ${WORK_DIR}_all
-
-# CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_PATH}/dist_train.sh \
-#     ${CONFIG_FILE} \
-#     ${N_GPU} \
-#     --work-dir ${WORK_DIR}_all \
-#     --cfg-options data.train.ann_file=${TRAIN_ROOT}.json
-
-# CUDA_VISIBLE_DEVICES=${VIS_GPU} PORT=${GPU_PORT} ./${TOOL_PATH}/dist_test.sh \
-#     ${CONFIG_FILE} \
-#     ${WORK_DIR}_all/latest.pth \
-#     ${N_GPU} \
-#     --eval bbox \
-#     --out ${WORK_DIR}_all/test2.pkl 2>&1 | tee ${RES_ROOT}_all.txt
-
-# rm ${WORK_DIR}_all/*.log
-# rm ${WORK_DIR}_all/epoch_[1-9].pth
-# rm ${WORK_DIR}_all/epoch_10.pth
-# rm ${WORK_DIR}_all/epoch_11.pth
+rm ${_WORK_DIR}/*.log
+rm ${_WORK_DIR}/*.pkl
+rm ${_WORK_DIR}/epoch_[1-9].pth
+rm ${_WORK_DIR}/epoch_10.pth
+rm ${_WORK_DIR}/epoch_11.pth
