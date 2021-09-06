2047216731b9def39e7a404ed55708eaae96544a
Thaddäus Wiedemer <thaddaeus.wiedemer@gmail.com>
2021-09-06 17:45:25 +0800

    modularized GPA and validated implementation

--- train/tuning_batch.sh
+++ train/tuning_batch.sh
@@ -221,5 +221,6 @@ cd ~/MasterthesisCode
 
 # improved framework
 # train/_gpa_coco_piropo_tuning.sh 40 20 a TwoStageDetectorDA GPAtest # check validity of framework
+train/_gpa_coco_piropo_tuning.sh 40 20 a TwoStageDetectorDA GPAtest1 # check validity of framework
 # train/_gpa_coco_piropo_tuning.sh 40 20 a TwoStageDetectorDA GPAratio # don't keep ratio
-train/_gpa_coco_piropo_tuning.sh 40 20 a TwoStageDetectorDA GPAratiotest # don't keep ratio, even durng testing
+# train/_gpa_coco_piropo_tuning.sh 40 20 a TwoStageDetectorDA GPAratiotest # don't keep ratio, even durng testing
